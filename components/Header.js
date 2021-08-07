99c95feea20782f8a96ae94de1a8cb0f4269b2b6
Basil Khan <basilkhan76@gmail.com>
2021-08-07 08:04:15 -0400

    Working on header search bar

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50">
+    <header
+      className="sticky top-0 z-50 grid grid-cols-3 
+    bg-white shadow-md p-5 md:px-10"
+    >
       {/* left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
@@ -14,7 +17,9 @@ function Header() {
       </div>
 
       {/* Middle */}
-      <div></div>
+      <div>
+        <input type="text" />
+      </div>
 
       {/* Right */}
       <div></div>
