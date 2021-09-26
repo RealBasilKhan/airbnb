00a5777d31d6a11740597bdde3c3e7c5b4579e2b
Basil Khan <basilkhan76@gmail.com>
2021-09-26 11:45:05 -0400

    Map Added Along with popups

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import getCenter from "geolib/es/getCenter";
 
 function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   //Transform the search results object into the { latitude, longitude } object
   const coordinates = searchResults.map((result) => ({
     longitude: result.long,
@@ -20,13 +22,46 @@ function Map({ searchResults }) {
     zoom: 11,
   });
 
+  console.log(selectedLocation);
+
   return (
     <ReactMapGL
       mapStyle="mapbox://styles/basilk76/cku0jzx0810tf18o6rc8x669s"
       mapboxApiAccessToken={process.env.mapbox_key}
       {...viewport}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    ></ReactMapGL>
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetTop={-15}
+            offsetLeft={-15}
+          >
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="location-pin"
+            >
+              📍
+            </p>
+          </Marker>
+
+          {selectedLocation.long === result.long ? (
+            <Popup
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
   );
 }
 
